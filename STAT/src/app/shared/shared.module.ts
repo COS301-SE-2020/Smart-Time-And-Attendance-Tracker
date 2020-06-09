343a89c406c70b8894f42d7a413f4cba6f5f996a
Vianka <u18278273@tuks.co.za>
2020-06-09 17:33:26 +0200

    Added startup components and router linking
    Startup pages will be displayed when the app is launched. This consists of Home, Features, STAT Desktop, Sign Up and Sign In.
    The linking to these pages was also added to the navbar.

--- STAT/src/app/shared/shared.module.ts
+++ STAT/src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class SharedModule { }
