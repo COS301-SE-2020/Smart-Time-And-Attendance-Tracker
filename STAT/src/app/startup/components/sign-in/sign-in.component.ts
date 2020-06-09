181bc96f8848e37cfb3c6cdecc744b549bd50947
Vianka <u18278273@tuks.co.za>
2020-06-09 22:59:44 +0200

    Added sign in form and styling

--- STAT/src/app/startup/components/sign-in/sign-in.component.ts
+++ STAT/src/app/startup/components/sign-in/sign-in.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.sass']
 })
 export class SignInComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
   }
 
 }
