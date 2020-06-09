dfee36a8c7091fb236cb39e3b4c7e5a1d07fd930
Vianka <u18278273@tuks.co.za>
2020-06-09 17:03:50 +0200

    Created shared header component
    This header will appear at the top of the startup pages, acting as a navigation bar

---
+++ STAT/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
