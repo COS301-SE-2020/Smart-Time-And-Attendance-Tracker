12177ae4aadf438b9592fce2b2fcee0e6f315db6
Vedha286 <u18020624@tuks.co.za>
2020-06-11 16:38:50 +0200

    adding intital database

---
+++ STAT/server/controller/organisation.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+const OrganisationModel = mongoose.model("Organisation");
+router.get("/", (req, res)=>{
+    res.send("Organisation controller");
+});
+
+
+router.post("/add", (req, res) => {
+    var organisation = new OrganisationModel();
+    organisation.ID = db.Organisation.find().Count()+1;
+    organisation.OrganisationName = req.body.organisationName;
+    organisation.SystemAdminstrator = [];
+    organisation.SecurityAdminstrator = [];
+    organisation.DataAnalyst = [];
+    organisation.save((err, doc) => {
+        if(!err){
+            res.send("Created Organisation");
+        }
+        else{
+            res.send("Error Occured");
+        }
+    })
+});
+
+module.exports = router;
