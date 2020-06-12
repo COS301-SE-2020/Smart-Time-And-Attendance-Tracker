114afe75feff834aa65d75cea41c53e43a8239ba
jesse <51156224+chirgugh@users.noreply.github.com>
2020-06-12 09:03:45 +0200

    api login get request

--- STAT/controller/user.js
+++ STAT/controller/user.js
@@ -1,18 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const router = express.Router();
 const UserModel = mongoose.model("User");
-router.get("/", (req, res)=>{
-    res.send("User controller");
+
+
+
+
+var assert = require('assert');
+router.get("/api/login", (req, res)=>{//// only returning all user details in the DB 
+////Access DB
+    var allUsersDetails = [];
+    var user = new UserModel();
+    var url="mongodb://localhost:27017/STAT";
+    mongoose.connect(url,function(err, db){
+        assert.equal(null, err);
+        var cursor = db.collection('users').find();
+        cursor.forEach(function(doc, err) {
+          assert.equal(null, err);
+          allUsersDetails.push(doc);
+        }, function() {
+          db.close();
+          console.log(allUsersDetails);
+          res.send(allUsersDetails);
+        });
+      })
+    .catch(error=> console.log(error)) 
+//Search  - (user with this email doesnt exist)
+
+//Authenitcate email and password (Consult Vedha on hashing & salting algorithm choice)
+
+//Return name,email,profile pic path
+
+//Cookies?
+ 
 });
 
 router.post("/add", (req, res) => {
     var user = new UserModel();
     user.ID = db.User.find().Count()+1;
-    user.ProfileName = req.body.profileName;
-    user.ProfilePicture = req.body.profilePicturePath;
-    user.Password = req.body.password;
     user.Name = req.body.name;
     user.Surname = req.body.surname;
     user.Email = req.body.email;
@@ -24,7 +49,15 @@ router.post("/add", (req, res) => {
         else{
             res.send("Error Occured");
         }
-    });
+    })
 });
+
+
+
+
+router.post("/api/update", (req, res) => {
+    res.send("Under Construction");
+});
+
 
 module.exports = router;
