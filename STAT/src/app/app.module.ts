ae4ee9d25c6b24841bbc8517a7cf66bc1fab0b8a
Vianka <u18278273@tuks.co.za>
2020-06-22 17:49:47 +0200

    Added form validation
    Form validation and error messages were added to the sign in and sign up forms

--- STAT/src/app/app.module.ts
+++ STAT/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +25,8 @@ import {HttpClientModule} from '@angular/common/http';
     MaterialComponentsModule,
     FormsModule,
     DashboardModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- STAT/src/app/startup/components/sign-up/sign-up.component.ts
+++ STAT/src/app/startup/components/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup } from '@angular/forms';
 import { AccountManagementService } from 'src/app/shared/services/account-management.service';
 import { Router } from '@angular/router';
 
@@ -9,6 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./sign-up.component.sass']
 })
 export class SignUpComponent implements OnInit {
+
+  signUpForm : FormGroup
+  signInForm : FormGroup
 
   constructor(public service : AccountManagementService, public router : Router) { }
   
@@ -24,7 +27,85 @@ export class SignUpComponent implements OnInit {
     signInButton.addEventListener('click', () => {
       container.classList.remove("right-panel-active");
     });
+
+    // sign up form
+    this.signUpForm = new FormGroup({
+      name : new FormControl('', [Validators.required]),
+      surname : new FormControl('', [Validators.required]),
+      email : new FormControl('', [Validators.required, Validators.email]),
+      password : new FormControl('', [Validators.required, Validators.minLength(8)]),
+      passwordConf : new FormControl('', [Validators.required])
+    });
+
+    this.signUpForm.setValidators(this.MustMatch('password', 'passwordConf'));
+
+    // sign in form
+    this.signInForm = new FormGroup({
+      email : new FormControl('', [Validators.required, Validators.email]),
+      password : new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
   }
+
+  /**************
+  FORM VALIDATION
+  ***************/
+
+  // get form control
+  get su() { return this.signUpForm.controls; }
+  get si() { return this.signInForm.controls; }
+
+  // display errors for email
+  getEmailError() {
+    if (this.signUpForm.controls.email.hasError('required')) {
+      return 'Please enter a value';
+    }
+
+    return this.signUpForm.controls.email.hasError('email') ? 'Invalid email address' : '';
+  }
+
+  // display errors for password
+  getPassError() {
+    if (this.signUpForm.controls.password.hasError('required')) {
+      return 'Please enter a value';
+    }
+
+    return this.signUpForm.controls.password.hasError('minlength') ? 'Invalid password length' : '';
+  }
+
+  // display errors for confirm password
+  getConfError() {
+    if (this.signUpForm.controls.passwordConf.hasError('required')) {
+      return 'Please enter a value';
+    }
+
+    return this.signUpForm.controls.passwordConf.hasError('mustMatch') ? 'Passwords do not match' : '';
+  }
+
+  // validate that passwords match
+  MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+
+        if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+            // return if another validator has already found an error on the matchingControl
+            return;
+        }
+
+        // set error on matchingControl if validation fails
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ mustMatch: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+
+        return null;
+    }
+  }
+
+  /********
+  API CALLS
+  *********/
   
   // submit sign up form
   signUp(form : NgForm) {
