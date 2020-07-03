9bda2f93d8de98348bdf8726a408c5ab0b3888d8
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-03 18:39:16 +0200

    adding time entry to user model

---
+++ STAT-server/models/timeEntry.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const TimeEntrySchema = mongoose.Schema({
+  TimeID: { 
+    type: Integer,
+    required : "ID required.",
+    unique: true
+  },
+  TaskID: {
+    type: String,
+    required : "Task required.",
+  },
+  Duration: { 
+    type: String,
+    required: "Duration required."
+  },
+  Date: { 
+    type: Date,
+    required: "Date required."
+  },
+  Description: { 
+    type: String,
+    required: "Description of time entry is required."
+  },
+  Device: { 
+    type: String,
+    required: "Device required."
+  }
+});
+
+module.exports = mongoose.model('TimeEntry', TimeEntrySchema);
--- STAT-server/models/user.model.js
+++ STAT-server/models/user.model.js
@@ -34,7 +34,13 @@ var UserSchema = new mongoose.Schema({
     Authenticate:{
         type: Boolean,  
         required : "Authentication required."
-    }
+    },
+    TimeEntry: [
+        { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'TimeEntry' 
+        }
+    ]
 });
 
 //Methods
