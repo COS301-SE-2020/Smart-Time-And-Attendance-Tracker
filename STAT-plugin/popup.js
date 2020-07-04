3ab56b18705c76e1a57728929ed1cde1d1a9b8e9
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-04 19:53:46 +0200

    send api call function added(not working yet)

--- STAT-plugin/popup.js
+++ STAT-plugin/popup.js
@@ -4,11 +4,14 @@ function showTime() {
     var desc = document.getElementById("desc");
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         currentID = tabs[0].id;
-        var url = chrome.extension.getBackgroundPage().History[currentID][0][1];
-        url = url.split("://")[1];
-        url = url.split("/")[0];
-        desc.innerHTML = url + ": ";
-        desc.innerHTML += FormatDuration(now - chrome.extension.getBackgroundPage().History[currentID][0][0]) + "\n";
+        if(chrome.extension.getBackgroundPage().History[currentID][0][0] != -1)
+        {
+            var url = chrome.extension.getBackgroundPage().History[currentID][0][1];
+            url = url.split("://")[1];
+            url = url.split("/")[0];
+            desc.innerHTML = url + ": ";
+            desc.innerHTML += FormatDuration(now - chrome.extension.getBackgroundPage().History[currentID][0][0]) + "\n";
+        }
     });    
 }
 
@@ -24,10 +27,46 @@ stopTimer.onclick = function(){
         url = url.split("/")[0];
         desc.innerHTML = url + ": ";
         desc.innerHTML += FormatDuration(now - chrome.extension.getBackgroundPage().History[currentID][0][0]) + "\n";
+        console.log("Stoped timer. Saving data");
+        SaveTime(url, now, chrome.extension.getBackgroundPage().History[currentID][0][0]);
         chrome.extension.getBackgroundPage().History[currentID][0][0] = -1;
         startTimer.style.display = "block";
         stopTimer.style.display = "none";
+        
     });   
+}
+var socket = io();
+socket.on('connect', function () { 
+			
+})
+
+function SaveTime(url,startTime, endTime) {
+    var http = new XMLHttpRequest();
+    var url = '/api/userTimeEntry/';
+   
+    var data = new FormData();
+    data.append('Description', url);
+    data.append('StartTime', startTime);
+    data.append('EndTime', endTime);
+    data.append('TaskID', "abcd1234");
+    data.append('Device', "Browser");
+    data.append('Date', new Date());
+    console.log("Saving data");
+    console.log(data);
+    http.open('POST', url, true);
+
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+        }
+    }
+    http.send(data);
+}
+
+function Save() {
+
 }
 
 
