ca7328abed18fd2b4734dffc45a228344e2f8bbe
Munashe <u18078975@tuks.co.za>
2020-07-05 11:20:09 +0200

    Dashboard - Today screens

--- STAT/src/app/dashboard/today/today.component.ts
+++ STAT/src/app/dashboard/today/today.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-today',
   templateUrl: './today.component.html',
-  styleUrls: ['./today.component.scss']
+  styleUrls: ['./today.component.sass']
 })
 export class TodayComponent implements OnInit {
 
