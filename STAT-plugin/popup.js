39f4585f57c3ea2e3e0a2f1f5f1ab0db95fb9510
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-08 13:15:39 +0200

    added start tracking function

--- STAT-plugin/popup.js
+++ STAT-plugin/popup.js
@@ -32,11 +32,24 @@ stopTimer.onclick = function(){
         chrome.extension.getBackgroundPage().History[currentID][0][0] = -1;
         startTimer.style.display = "block";
         stopTimer.style.display = "none";
-        
     });   
 }
 
-
+startTimer.onclick = function(){
+    var now  = new Date();
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        currentID = tabs[0].id;
+        var url = chrome.extension.getBackgroundPage().History[currentID][0][1];
+        url = url.split("://")[1];
+        url = url.split("/")[0];
+        desc.innerHTML = url + ": ";
+        desc.innerHTML += FormatDuration(now - chrome.extension.getBackgroundPage().History[currentID][0][0]) + "\n";
+        console.log("Started tracking again  " + url);
+        chrome.extension.getBackgroundPage().History[currentID][0][0] = now;
+        stopTimer.style.display = "block";
+        startTimer.style.display = "none";
+    });   
+}
 
 setInterval(showTime, 1000);    //calling function every second
 
