22dc6b5e2c8cbb865b4a44e1935e6b41f7c1771c
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-08 12:44:51 +0200

    moved updateTimeEntryPeriodically function to common

--- STAT-plugin/common.js
+++ STAT-plugin/common.js
@@ -1,61 +1,41 @@
 login();
 var token = "";
-function login(url,startTime, endTime) {
-    console.log("hello req");
+function login() {
     var http = new XMLHttpRequest();
     var url = 'http://localhost:3000/api/user/login';
-    
-    var data = {
-        'Description': url
-    };
-    console.log("Saving data");
-    console.log(data);
     http.open('POST', url, true);
-
     http.setRequestHeader('Content-type', 'application/json');
     http.setRequestHeader("authorization", "token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMDA3NDI5ZjFjYzlhNDViYzk4YjQ5YSIsImF1dGhlbnRpY2F0ZSI6dHJ1ZSwicm9sZXMiOls1LDNdLCJpYXQiOjE1OTM4NjY1MzYsImV4cCI6MTU5Mzk1MjkzNn0.Gzj8FIIcmyQHaEUXZBSQF7fVez1_41gz42DIY-xNQcs");
-    console.log("OPENED  " + http.readyState);  // readyState will be 1
     http.onreadystatechange = function() {
-        console.log("http.readyState " + http.readyState);
-        console.log("http.status " + http.status);
         if(http.readyState == 4 && http.status == 200) {
-            console.log(http.responseText);
             var data = JSON.parse(http.responseText);
             token = data.token;
         }
         else if(http.readyState == 4 && http.status == 500) {
-            alert(http.responseText);
+            console.log(http.responseText);
         }
         else if(http.readyState == 4 && http.status == 200) {
-            alert(http.responseText);
+            console.log(http.responseText);
         }
     }
-
-    http.onprogress = function () {
-        console.log('LOADING', http.readyState); // readyState will be 3
-    };
-    
-    http.onload = function () {
-        console.log('DONE', http.readyState); // readyState will be 4
-    };
     var text = '{ "email":"tester1@gmail.com" , "password":"Abcd@1234" }';
     http.send(text);
 } 
 
 
 function AddTimeEntry(url,startTime, endTime) {
-    console.log("startTime " + startTime + "   endTime " + endTime);
+    console.log("startTime " + startTime.getTime() + "   endTime " + endTime.getTime());
     var http = new XMLHttpRequest();
-    var url = 'http://localhost:3000/api/userTimeEntry/addTimeEntry';
+    var apiURL = 'http://localhost:3000/api/userTimeEntry/addTimeEntry';
     var text = '{ "Description": "'+ url + '",'
-        + '"StartTime": "'+ startTime + '",' 
-        + '"EndTime": "'+ endTime + '",' 
+        + '"StartTime": "'+ startTime.getTime() + '",' 
+        + '"EndTime": "'+ endTime.getTime() + '",' 
         + '"TaskID": "abcd1234",' 
         + '"Device": "Browser",' 
         + '"Date": "'+ new Date() + '"' 
         + '}';
 
-    http.open('POST', url, true);
+    http.open('POST', apiURL, true);
 
     http.setRequestHeader('Content-type', 'application/json');
     http.setRequestHeader("authorization", "token "+token);
@@ -69,3 +49,19 @@ function AddTimeEntry(url,startTime, endTime) {
     }
     http.send(text);
 }
+
+function updateTimeEntryPeriodically()
+{
+    var now  = new Date();
+    console.log("TABS: ");
+    for(tabID in chrome.extension.getBackgroundPage().History) {
+        console.log("tab ID " + tabID);
+        var url = chrome.extension.getBackgroundPage().History[tabID][0][1];
+        url = url.split("://")[1];
+        url = url.split("/")[0];
+        console.log("Saving data  " + chrome.extension.getBackgroundPage().History[tabID][0][0]);
+        AddTimeEntry(url, chrome.extension.getBackgroundPage().History[tabID][0][0], now);    
+    }
+}
+
+setInterval(updateTimeEntryPeriodically, 60*1000); //calling function every minute (60 seconds)
--- STAT-plugin/popup.js
+++ STAT-plugin/popup.js
@@ -28,7 +28,7 @@ stopTimer.onclick = function(){
         desc.innerHTML = url + ": ";
         desc.innerHTML += FormatDuration(now - chrome.extension.getBackgroundPage().History[currentID][0][0]) + "\n";
         console.log("Stoped timer. Saving data  " + chrome.extension.getBackgroundPage().History[currentID][0][0]);
-        AddTimeEntry(url, chrome.extension.getBackgroundPage().History[currentID][0][0].getTime(), now.getTime());
+        AddTimeEntry(url, chrome.extension.getBackgroundPage().History[currentID][0][0], now);
         chrome.extension.getBackgroundPage().History[currentID][0][0] = -1;
         startTimer.style.display = "block";
         stopTimer.style.display = "none";
@@ -36,19 +36,8 @@ stopTimer.onclick = function(){
     });   
 }
 
-function updateTimeEntryPeriodically()
-{
-    console.log("TABS: ");
-    for(tabID in chrome.extension.getBackgroundPage().History) {
-        console.log("tab ID " + tabID);
-        var url = chrome.extension.getBackgroundPage().History[tabID][0][1];
-        url = url.split("://")[1];
-        url = url.split("/")[0];
-        console.log("Saving data  " + chrome.extension.getBackgroundPage().History[tabID][0][0]);
-        AddTimeEntry(url, chrome.extension.getBackgroundPage().History[tabID][0][0].getTime(), (new Date()).getTime());    
-    }
-}
+
 
 setInterval(showTime, 1000);    //calling function every second
-setInterval(updateTimeEntryPeriodically, 60*1000); //calling function every minute (60 seconds)
+
 
