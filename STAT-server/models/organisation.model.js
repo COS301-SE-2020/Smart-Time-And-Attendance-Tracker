d6820788d665e106c751feafd8e9c922f53ab153
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-26 10:57:25 +0200

    Organisation model comments

--- STAT-server/models/organisation.model.js
+++ STAT-server/models/organisation.model.js
@@ -1,3 +1,24 @@
+/**
+  * @file organisation.model.js
+  * @author Vedha Krishna Velthapu, Jana Sander, Jesse
+  * @fileoverview This file contains the Organisation model in our database
+  * @date 11June 2020
+ */
+
+/**
+* Filename:             STAT-server/models/organisation.model.js
+*
+* Author:               Vedha Krishna Velthapu, Jana Sander, Jesse 
+*   
+* File Creation Date:   11 June 2020
+*
+* Development Group:    Visionary
+*
+* Project:              Smart Time and Attendance Tracker
+*
+* Description:          This file contains the Organisation model in our database
+*
+*/ 
 const mongoose = require("mongoose")
 
 var OrganisationSchema = new mongoose.Schema({
