09258e3887c738b74f27c120c213920515dbda73
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-26 10:58:52 +0200

    team model comments

--- STAT-server/models/team.model.js
+++ STAT-server/models/team.model.js
@@ -1,3 +1,24 @@
+/**
+  * @file team.model.js
+  * @author Vedha Krishna Velthapu, Jana Sander, Jesse
+  * @fileoverview This file contains the Team model in our database
+  * @date 11June 2020
+ */
+
+/**
+* Filename:             STAT-server/models/team.model.js
+*
+* Author:               Vedha Krishna Velthapu, Jana Sander, Jesse 
+*   
+* File Creation Date:   11 June 2020
+*
+* Development Group:    Visionary
+*
+* Project:              Smart Time and Attendance Tracker
+*
+* Description:          This file contains the Team model in our database
+*
+*/
 const mongoose = require("mongoose")
 
 var TeamSchema = new mongoose.Schema({
