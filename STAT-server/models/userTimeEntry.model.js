a334d8b4c1fb53f29e5776788a5f0420088de8c1
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-26 11:00:27 +0200

    UserTimeEntry model comments

--- STAT-server/models/userTimeEntry.model.js
+++ STAT-server/models/userTimeEntry.model.js
@@ -1,3 +1,24 @@
+/**
+  * @file userTimeEntry.model.js
+  * @author Vedha Krishna Velthapu, Jana Sander, Jesse
+  * @fileoverview This file contains the UserTimeEntry model in our database
+  * @date 3 July 2020
+ */
+
+/**
+* Filename:             STAT-server/models/userTimeEntry.model.js
+*
+* Author:               Vedha Krishna Velthapu, Jana Sander, Jesse 
+*   
+* File Creation Date:   3 July 2020
+*
+* Development Group:    Visionary
+*
+* Project:              Smart Time and Attendance Tracker
+*
+* Description:          This file contains the UserTimeEntry model in our database
+*
+*/
 const mongoose = require("mongoose")
 
 var UserTimeEntrySchema = new mongoose.Schema({
