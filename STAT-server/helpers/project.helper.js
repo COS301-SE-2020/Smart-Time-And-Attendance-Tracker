70de4132923aa1fc6a9de2cfd9d77ef5d301a4aa
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-26 11:12:37 +0200

    project.helper.js comments

--- STAT-server/helpers/project.helper.js
+++ STAT-server/helpers/project.helper.js
@@ -1,7 +1,36 @@
+/**
+  * @file STAT-server/helper/project.helper.js
+  * @author Vedha Krishna Velthapu, Jana Sander, Jesse
+  * @fileoverview This file handles some of the requests regarding Project model in our database. 
+  * This is a helper file to handle Project related requests.
+  * @date 15 July 2020
+ */
+
+/**
+* Filename:             STAT-server/helper/project.helper.js
+*
+* Author:               Vedha Krishna Velthapu, Jana Sander, Jesse 
+*   
+* File Creation Date:   15 July 2020
+*
+* Development Group:    Visionary
+*
+* Project:              Smart Time and Attendance Tracker
+*
+* Description:          This file handles some of the requests regarding Project model in our database. 
+*                       This is a helper file to handle Project related requests.
+*
+*/
 const mongoose = require("mongoose");
 const ProjectModel = mongoose.model("Project");
 const TaskHelper =require('../helpers/task.helper');
 
+/**
+ * This function assigns a team to a project.
+ * @param {String} id ID of project
+ * @param {String} teamID ID of team
+ * @param {*} done 
+ */
 module.exports.addTeam = (id, teamID, done)=>{
     ProjectModel.updateOne({_id: id},{Team: teamID},(err, result) => {
         if(err) 
@@ -14,6 +43,12 @@ module.exports.addTeam = (id, teamID, done)=>{
     });
 }
 
+/**
+ * This funtion returns the hourly rate of a project.
+ * @param {String} id ID of project.
+ * @param {*} done 
+ * @returns {Number} returns the hourly rate of a project. 
+ */
 module.exports.hourlyRate = (id, done)=>{
     ProjectModel.find({_id: id},(err, result) => {
         if(err) 
@@ -26,6 +61,13 @@ module.exports.hourlyRate = (id, done)=>{
     });
 }
 
+/**
+ * This function returns the tasks of the project, and additional details about the project such as, 
+ * project name, project ID, due date of project, and the hourly rate of the project.
+ * @param {String} id ID of project.
+ * @param {*} done Returns the tasks of the project, and additional details about the project such as, 
+ * project name, project ID, due date of project, and the hourly rate of the project.
+ */
 module.exports.getTasks = (id, done)=>{
     ProjectModel.findOne({ _id: id},{ Completed: false},(err, result) => {
         if(err) 
