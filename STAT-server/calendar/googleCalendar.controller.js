0ad80cb4052f452aff3a0ca0777a766aa5a88aef
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-27 20:39:15 +0200

    setting up google calendar controller

---
+++ STAT-server/calendar/googleCalendar.controller.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = '../config/token.json';
