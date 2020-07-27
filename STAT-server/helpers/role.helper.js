f2e8c8899b8299aea1f3b897468c6281c17e00d1
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-27 09:57:05 +0200

    Role helper comments

--- STAT-server/helpers/role.helper.js
+++ STAT-server/helpers/role.helper.js
@@ -1,6 +1,35 @@
+/**
+  * @file STAT-server/helper/role.helper.js
+  * @author Vedha Krishna Velthapu, Jana Sander, Jesse
+  * @fileoverview This file handles some of the requests regarding Role model in our database. 
+  * This is a helper file to handle Role related requests.
+  * @date 15 July 2020
+ */
+
+/**
+* Filename:             STAT-server/helper/role.helper.js
+*
+* Author:               Vedha Krishna Velthapu, Jana Sander, Jesse 
+*   
+* File Creation Date:   15 July 2020
+*
+* Development Group:    Visionary
+*
+* Project:              Smart Time and Attendance Tracker
+*
+* Description:          This file handles some of the requests regarding Role model in our database. 
+*                       This is a helper file to handle Role related requests.
+*
+*/
 const mongoose = require("mongoose");
 const RoleModel = mongoose.model("Role");
 
+ /**
+  * 
+  * @param {*} id ID of role.
+  * @param {*} done 
+  * @return {String} - Role of given ID.
+  */
 module.exports.getRole = (id, done)=>{
     RoleModel.findOne({ID: id},(err, result) => {
         if(err) 
