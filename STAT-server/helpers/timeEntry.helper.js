b13db916eba9cae6c3a010e8230162e43cead030
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2020-07-27 10:24:12 +0200

    timeEntry.helper comments

--- STAT-server/helpers/timeEntry.helper.js
+++ STAT-server/helpers/timeEntry.helper.js
@@ -1,6 +1,35 @@
+/**
+  * @file STAT-server/helper/timeEntry.helper.js
+  * @author Vedha Krishna Velthapu, Jana Sander, Jesse
+  * @fileoverview This file handles some of the requests regarding timeEntry model in our database. 
+  * This is a helper file to handle timeEntry related requests.
+  * @date 8 July 2020
+ */
+
+/**
+* Filename:             STAT-server/helper/timeEntry.helper.js
+*
+* Author:               Vedha Krishna Velthapu, Jana Sander, Jesse 
+*   
+* File Creation Date:   8 July 2020
+*
+* Development Group:    Visionary
+*
+* Project:              Smart Time and Attendance Tracker
+*
+* Description:          This file handles some of the requests regarding timeEntry model in our database. 
+*                       This is a helper file to handle timeEntry related requests.
+*
+*/
 const mongoose = require("mongoose");
 const TimeEntryModel = mongoose.model("TimeEntry");
 
+/**
+ * 
+ * @param {*} id ID of time entry.
+ * @param {*} done 
+ * @returns {Object} Time Entry document(object).
+ */
 module.exports.getTimeEntry = (id, done)=>{
     TimeEntryModel.findOne({ _id: id},(err, result) => {
         if(err) 
