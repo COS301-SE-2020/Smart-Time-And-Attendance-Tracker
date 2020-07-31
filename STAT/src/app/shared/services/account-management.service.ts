4a2186e7ad2b3043947c029e02bdcef18601b74b
Jana Sander <u18102639@tuks.co.za>
2020-07-31 16:41:45 +0200

    Changed getTasks to getProjects

--- STAT/src/app/shared/services/account-management.service.ts
+++ STAT/src/app/shared/services/account-management.service.ts
@@ -58,7 +58,7 @@ export class AccountManagementService {
   public getProjectsAndTasks(token){
     const headers = new HttpHeaders()
           .set('Content-Type', 'application/json').set( 'Authorization', "Bearer "+token);
-    return this.http.get(this.ROOT_URL+ 'user/getTasks', {
+    return this.http.get(this.ROOT_URL+ 'user/getProjects', {
       headers: headers
     });
   }
