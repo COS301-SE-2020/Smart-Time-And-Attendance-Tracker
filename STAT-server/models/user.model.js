93469f04b6065a62bef5ee1434a2aa94dc6b734e
Jana Sander <u18102639@tuks.co.za>
2020-08-18 10:45:19 +0200

    Revert "Removes server folder"
    This reverts commit 9ca4548713eba6e6dee3e57612ad2e0ba930aab4.

---
+++ STAT-server/models/user.model.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose")
+const bcrypt=require("bcrypt");
+const jwt = require('jsonwebtoken');
+
+var UserSchema = new mongoose.Schema({
+    ID:{
+        type: Number,
+        required : "ID required.",
+        unique: true
+    },
+    ProfilePicture:{
+        type: String
+    },
+    Password:{
+        type: String,  
+        required : "Password required."
+    },
+    Name:{
+        type: String,
+        required : "Name required."
+    },
+    Surname:{
+        type: String,
+        required : "Surname required."
+    },
+    Email:{
+        type: String,
+        required : "Email required.",
+        unique: true
+    },
+    Role:{
+        type: Array
+    }
+});
+
+//Methods
+
+UserSchema.methods.verifyPassword = function(password){
+    return bcrypt.compareSync(password, this.Password);
+};
+
+UserSchema.methods.generateJWT = function() {
+    return jwt.sign({id: this.ID, roles: this.Role},
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXP
+        });
+}
+
+UserSchema.pre('save',function(next){
+    bcrypt.genSalt(10,(err,salt) => {
+       bcrypt.hash(this.Password,salt,(err,hash) => {
+           this.Password=hash;
+           this.saltedsecret=salt;
+           next();
+       });
+    });
+});
+
+mongoose.model("User", UserSchema);
