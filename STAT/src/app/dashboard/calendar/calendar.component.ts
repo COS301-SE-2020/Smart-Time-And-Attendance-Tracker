71e453a7ffd257c5db039198bd8f94dee881351a
munasheghub <54100883+munasheghub@users.noreply.github.com>
2020-08-18 17:48:53 +0200

    Updated calendar authorisation

--- STAT/src/app/dashboard/calendar/calendar.component.ts
+++ STAT/src/app/dashboard/calendar/calendar.component.ts
@@ -14,7 +14,6 @@ export class CalendarComponent implements OnInit {
 
   ngOnInit(): void {
     this.authorised = false;
-    gapi
     this.checkAuth();
   }
 
@@ -23,15 +22,46 @@ export class CalendarComponent implements OnInit {
     var apiURL = "http://localhost:3000/api/calendar/getCredentials?"+ "calendar=google";
     http.open("GET",apiURL , true);
     http.setRequestHeader('Content-type', 'application/json');
-    http.setRequestHeader("Authorization", "Bearor "+ "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMjJiODM1ZGM5YmIyNDJmNGUyN2E3OSIsImlhdCI6MTU5NzQ4ODQyMywiZXhwIjoxNTk3NTc0ODIzfQ.v0mxh5Yp_iY9aOqc7uLHyrLTWZxtigbqdB4BKWH8PyI");
+    http.setRequestHeader("Authorization", "Bearor "+ localStorage.getItem('token'));//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMjJiODM1ZGM5YmIyNDJmNGUyN2E3OSIsImlhdCI6MTU5NzQ4ODQyMywiZXhwIjoxNTk3NTc0ODIzfQ.v0mxh5Yp_iY9aOqc7uLHyrLTWZxtigbqdB4BKWH8PyI");
     http.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         var response = JSON.parse(this.responseText);
         gapi.client.setApiKey(response.apiKey);
-        gapi.auth.authorize({client_id: response.clientId, scope: response.scopes, immediate: true}, this.handleAuthResult);
+        const authResult = gapi.auth.authorize({client_id: response.clientId, scope: response.scopes, immediate: true});
+
+        // handleAuthResult
+        var authorizeButton = document.getElementById('authorize-button');
+        if (authResult && !authResult.error) {
+          console.log('pig');
+          authorizeButton.style.visibility = 'hidden';
+          //makeApiCall
+          const http = new XMLHttpRequest();
+          var apiURL = "http://localhost:3000/api/calendar/syncEvents" ;
+          var params =  {calendar :"google", accessToken: authResult.access_token ,expiryDate: authResult.expires_at,tokenType: authResult.token_type };
+
+          http.open("POST",apiURL , true);
+          http.setRequestHeader('Content-type', 'application/json');
+          http.setRequestHeader("Authorization", "Bearor "+ "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMjJiODM1ZGM5YmIyNDJmNGUyN2E3OSIsImlhdCI6MTU5NzQ4ODQyMywiZXhwIjoxNTk3NTc0ODIzfQ.v0mxh5Yp_iY9aOqc7uLHyrLTWZxtigbqdB4BKWH8PyI");
+          http.onreadystatechange = function() {
+            if(this.readyState == 4 && this.status == 200) {
+              console.log(response);
+              var response = JSON.parse(this.responseText);
+            }
+            else if(this.readyState == 4)
+              //document.getElementById('error').innerHTML = response.message;
+              console.log(response.message);
+          }
+          http.send(JSON.stringify(params));
+
+        } else {
+          authorizeButton.style.visibility = '';
+          authorizeButton.onclick = this.handleAuthClick;
+        }
+
+        // makeApiCall
+
       }
-      else if(this.readyState == 4)
-        console.log(response);
+      else if(this.readyState == 4) {}
         //document.getElementById('error').innerHTML = response.message;
     }
     http.send();
@@ -40,6 +70,7 @@ export class CalendarComponent implements OnInit {
   handleAuthResult(authResult) {
     var authorizeButton = document.getElementById('authorize-button');
     if (authResult && !authResult.error) {
+      console.log('pig');
       authorizeButton.style.visibility = 'hidden';
       this.makeApiCall(authResult);
     } else {
@@ -57,7 +88,7 @@ export class CalendarComponent implements OnInit {
     http.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         var response = JSON.parse(this.responseText);
-        gapi.auth.authorize({client_id: response.clientId, scope: response.scopes, immediate: false}, this.handleAuthResult);
+        gapi.auth.authorize({client_id: response.clientId, scope: response.scopes, immediate: false});
       }
       else if(this.readyState == 4)
         console.log(response);
