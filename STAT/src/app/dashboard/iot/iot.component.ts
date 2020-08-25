856d996098ab0ba33cd8308eec8d90a433ba1799
munasheghub <54100883+munasheghub@users.noreply.github.com>
2020-08-25 17:31:24 +0200

    Revert "IOT screen design"
    This reverts commit 9d382269ea3a6c961e142db327cd9cde98a0a40c.

--- STAT/src/app/dashboard/iot/iot.component.ts
+++ STAT/src/app/dashboard/iot/iot.component.ts
@@ -7,19 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IOTComponent implements OnInit {
 
-  roles : string[] = ['Data Analyst', 'System Administrator', 'Security Administrator', 'Team Leader', 'General Team Member']
-  devices : any;
-
   constructor() { }
 
   ngOnInit(): void {
-    this.devices = [
-      {'deviceName': 'Left Camera - Lobby', 'macAddress': '111.203.75', 'description': 'Block B'},
-      {'deviceName': 'Card scanner - Reception', 'macAddress': '273.895.23', 'description': 'Admin Building'},
-      {'deviceName': 'Camera - Parking Entrance', 'macAddress': '888.759.28', 'description': 'Basement level B'},
-      {'deviceName': 'Fingerprint Scanner', 'macAddress': '985.346.89', 'description': 'VIP Offices'},
-      {'deviceName': 'Iris Scanner', 'macAddress': '995.274.78', 'description': 'Vault'},
-    ];
   }
 
 }
