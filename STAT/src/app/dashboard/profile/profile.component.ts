a894439ed33818d4ab0224866664cddd957aac2c
Vianka <u18278273@tuks.co.za>
2020-09-10 19:48:33 +0200

    Updated profile screen design

--- STAT/src/app/dashboard/profile/profile.component.ts
+++ STAT/src/app/dashboard/profile/profile.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  name : string
+  surname : string
+  email : string
+  roles : any 
+  profilePic : string = '../../../assets/proPic1.jpg'
+  cover : string = '../../../assets/STAT-logo.png'
+
   constructor() { }
 
   ngOnInit(): void {
+    this.name = localStorage.getItem('name')
+    this.surname = localStorage.getItem('surname')
+    this.roles = localStorage.getItem('roles')
+    this.profilePic = ''
+  }
+
+  openFile() {
+    document.querySelector('input').click()
+  }
+
+  handle(e){
+    console.log('Change input file')
   }
 
 }
