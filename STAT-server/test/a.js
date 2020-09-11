f244febf00f7b512dd2a88d6741f4d1508471b64
chirgugh <u17306192@tuks.co.za>
2020-09-11 08:09:12 +0200

    working user tests

---
+++ STAT-server/test/a.js
@@ -0,0 +1,87 @@
+//var server = require ('../index.js'),
+var assert = require ('assert')
+//var http = require ('http');
+
+
+let chai =require('chai')
+let chaiHttp =require('chai-http');
+
+let should =chai.should()
+let expect =chai.expect()
+
+chai.use(chaiHttp)
+const request = require("supertest");
+
+//server.listen();
+
+
+//require ('./project.js')
+//require ('./cover.js')
+//require ('./test')
+//require ('./userTimeEntries')
+//require ('./z.js')
+
+
+describe('test file 1', function() {
+/*
+    before(function () {
+       server.listen();
+    });
+
+    after(function () {
+        server.close();
+    }); 
+*/
+    
+    describe('test USER', function() {
+      require ('./user.js')
+    })
+
+    /*
+
+    describe('test USER TIME ENTRIES', function() {
+      require ('./userTimeEntries.js')
+    })
+
+    describe('test PROJECT', function() {
+      require ('./project.js')
+    })
+
+    describe('test TEAM', function() {
+      require ('./team.js')
+    })
+
+    describe('test ROLE', function() {
+      require ('./role.js')
+    })
+
+    describe('test ROLE', function() {
+      require ('./task.js')
+    })
+
+  
+
+    describe('iotDevice', function() {
+        require ('./iotDevice.js')
+      })
+*/
+
+
+      
+   /*
+    
+      describe('test file 2', function() {
+        require ('./cover.js')
+      })
+    
+      describe('test file 2', function() {
+        require ('./userTimeEntries.js')
+      })
+    
+      describe('test file 2', function() {
+        require ('./z.js')
+        //server.close();
+      })
+*/
+
+  })
