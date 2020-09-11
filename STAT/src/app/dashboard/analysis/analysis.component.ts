a67edf23fbc6612592e2a8b8b39e2e39574301b9
Vianka213 <u18278273@tuks.co.za>
2020-09-11 13:26:16 +0200

    Added performance (daily number of hours) chart

--- STAT/src/app/dashboard/analysis/analysis.component.ts
+++ STAT/src/app/dashboard/analysis/analysis.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit,ChangeDetectionStrategy,ChangeDetectorRef} from '@angu
 import { AnalysisService } from 'src/app/shared/services/analysis.service';
 import { HeaderService } from 'src/app/shared/services/header.service';
 
+// chart.js
+import { Chart } from 'chart.js';
+
 @Component({
   selector: 'app-analysis',
   templateUrl: './analysis.component.html',
@@ -16,6 +19,20 @@ export class AnalysisComponent implements OnInit {
   meView : boolean = true;
   projectView : boolean = false;
 
+  date : Date = new Date()
+  date1 : Date = new Date()
+  date2 : Date = new Date()
+  date3 : Date = new Date()
+  date4 : Date = new Date()
+  date5 : Date = new Date()
+  date6 : Date = new Date()
+  dates : any[] = []
+
+  // performance - daily number of hours
+  dailyValues : any[] = [0, 0, 0, 0, 0, 0, 0]
+  dailyChart : []
+  meanDailyHours : number = 0
+
   constructor(private cd: ChangeDetectorRef, public aService: AnalysisService, public headerService: HeaderService) { }
 
   ngOnInit(): void {
@@ -28,6 +45,23 @@ export class AnalysisComponent implements OnInit {
     this.getWeeklyTasksTimes();
     this.getProjectMembersTotalTime("5f3d4cdc5f704424503cff44");
     this.getPredictionsForWeekForProjects();
+
+    // set dates
+    this.date1.setDate(this.date.getDate()-1)
+    this.date2.setDate(this.date.getDate()-2)
+    this.date3.setDate(this.date.getDate()-3)
+    this.date4.setDate(this.date.getDate()-4)
+    this.date5.setDate(this.date.getDate()-5)
+    this.date6.setDate(this.date.getDate()-6)
+
+    this.dates.push(this.formatDate(this.date))
+    this.dates.push(this.formatDate(this.date1))
+    this.dates.push(this.formatDate(this.date2))
+    this.dates.push(this.formatDate(this.date3))
+    this.dates.push(this.formatDate(this.date4))
+    this.dates.push(this.formatDate(this.date5))
+    this.dates.push(this.formatDate(this.date6))
+
   }
 
   toggleAnalysis()
@@ -51,7 +85,56 @@ export class AnalysisComponent implements OnInit {
   {
     this.aService.getDailyValues(localStorage.getItem('token')).subscribe((data) => {
       console.log(data);
-    
+      let daily = data['totalDailyValues']
+      daily.forEach((element : any) => {
+        if (element['_id'] == this.dates[0]) {
+          this.dailyValues[0] = element['totalTime'] / 60
+        }
+
+        if (element['_id'] == this.dates[1]) {
+          this.dailyValues[1] = element['totalTime'] / 60
+        }
+
+        if (element['_id'] == this.dates[2]) {
+          this.dailyValues[2] = element['totalTime'] / 60
+        }
+
+        if (element['_id'] == this.dates[3]) {
+          this.dailyValues[3] = element['totalTime'] / 60
+        }
+
+        if (element['_id'] == this.dates[4]) {
+          this.dailyValues[4] = element['totalTime'] / 60
+        }
+
+        if (element['_id'] == this.dates[5]) {
+          this.dailyValues[5] = element['totalTime'] / 60
+        }
+
+        if (element['_id'] == this.dates[6]) {
+          this.dailyValues[6] = element['totalTime'] / 60
+        }
+      });
+      console.log(this.dailyValues)
+
+      this.dailyValues.forEach((element : any) => {
+        this.meanDailyHours += element
+      });
+      this.meanDailyHours = this.meanDailyHours / 7
+      this.meanDailyHours = Math.round((this.meanDailyHours + Number.EPSILON) * 100) / 100
+
+      // create chart
+      this.dailyChart = new Chart(
+        'chart', {
+          type: 'line',
+          data: { 
+            datasets: [{
+              data: this.dailyValues
+            }],
+            labels: this.dates
+          }
+        }
+      )
     },
     error => {
       console.log(error);
@@ -174,5 +257,25 @@ export class AnalysisComponent implements OnInit {
         this.headerService.kickOut();
       }
     });
+  }
+
+  formatDate(date : Date) {
+    var y = date.getFullYear().toString();
+    var m = (date.getMonth()+1).toString();
+    var d = date.getDate().toString();
+
+    let toReturn = new String(y + '/');
+
+    if (m.length == 1)
+      toReturn += ('0' + m + '/')
+    else
+      toReturn += (m + '/')
+
+    if (d.length == 1)
+      toReturn += ('0' + d)
+    else
+      toReturn += (d)
+
+    return toReturn
   }
 }
