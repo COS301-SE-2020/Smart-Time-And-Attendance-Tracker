56931fb60aa4c4efa6bbb38850431ac422dda965
Jana-git <u18102639@tuks.co.za>
2020-09-12 11:10:23 +0200

    UI elements for IOT camera mocking

--- mockIOT/src/app/app.component.ts
+++ mockIOT/src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+
+class User {
+  public id: string;
+  public tracking: boolean;
+
+  constructor(id: string) {
+    this.id = id;
+    this.tracking = false;
+  }
+
+  toggle() {
+    this.tracking = !this.tracking;
+  }
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
