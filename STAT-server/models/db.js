149fa6b928dd883f88376a48c92b90266872b4ee
munasheghub <54100883+munasheghub@users.noreply.github.com>
2020-09-13 23:38:53 +0200

    Fixed travis file

--- STAT-server/models/db.js
+++ STAT-server/models/db.js
@@ -23,7 +23,7 @@
 const mongoose = require('mongoose');
 require("../config/config.js");
 
-mongoose.connect(process.env.MONGODB_URI, (err) => {
+mongoose.connect(decodeURIComponent(process.env.MONGODB_URI), (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
