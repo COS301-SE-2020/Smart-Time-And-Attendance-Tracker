f3c50b54314bc34d4e3a7f8210fcfcbe7b641899
Vianka213 <u18278273@tuks.co.za>
2020-09-13 23:38:25 +0200

    Added profile picture uploading

--- STAT/src/app/dashboard/profile/profile.component.ts
+++ STAT/src/app/dashboard/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { AccountManagementService } from 'src/app/shared/services/account-management.service';
 import { HeaderService } from 'src/app/shared/services/header.service';
 
@@ -13,34 +16,58 @@ export class ProfileComponent implements OnInit {
   surname : string
   email : string
   roles : any 
-  profilePic : string = '../../../assets/proPic1.jpg'
+  profilePic : string = 'hi'
   cover : string = '../../../assets/STAT-logo.png'
   fileToUpload : File = null
 
-  constructor(public service : AccountManagementService, public headerService : HeaderService) { }
+  constructor(private storage : AngularFireStorage, public service : AccountManagementService, public headerService : HeaderService) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem('name')
     this.surname = localStorage.getItem('surname')
     this.roles = localStorage.getItem('roles')
-    this.profilePic = localStorage.getItem('profilePic')
+    //this.profilePic = localStorage.getItem('profilePic')
   }
 
   openFile() {
     document.querySelector('input').click()
   }
 
-  handle(file : any){
-    this.fileToUpload = file.item(0)
-    let location = "../../../assets/profilePictures/" + this.fileToUpload['name']
-    let save = this.name + this.surname
-    console.log(this.fileToUpload)
+  selectedFile: File = null;
+  fb;
+  downloadURL: Observable<string>;
+
+  handle(event : any){
+    var n = Date.now();
+    const file = event.target.files[0];
+    const filePath = `profilePictures/${n}`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(`profilePictures/${n}`, file);
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.downloadURL.subscribe(url => {
+            if (url) {
+              this.fb = url;
+              this.profilePic = url
+              this.addPic(url)
+            }
+          });
+        })
+      )
+      .subscribe(url => {
+        if (url) {
+          console.log(url);
+        }
+      });
   }
 
   //add profile picture
   addPic(pic : String) {
     let req ={"profilePicture": pic}
-    this.service.addProfilePic(localStorage.getItem('token'),req).subscribe((data) => {
+    this.service.addProfilePicture(localStorage.getItem('token'),req).subscribe((data) => {
       console.log(data);
 
     },
