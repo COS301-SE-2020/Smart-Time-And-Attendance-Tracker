0a23c7739fa9b2e2dc94cd84736b258106357bf5
Vianka213 <u18278273@tuks.co.za>
2020-09-14 08:38:34 +0200

    Added picture progress bar and dashboard link

--- STAT/src/app/dashboard/profile/profile.component.ts
+++ STAT/src/app/dashboard/profile/profile.component.ts
@@ -17,8 +17,9 @@ export class ProfileComponent implements OnInit {
   email : string
   roles : any 
   profilePic : string
-  cover : string = '../../../assets/STAT-logo.png'
+  cover : string 
   fileToUpload : File = null
+  uploadProgress: Observable<number>
 
   constructor(private storage : AngularFireStorage, public service : AccountManagementService, public headerService : HeaderService) { }
 
@@ -27,6 +28,13 @@ export class ProfileComponent implements OnInit {
     this.surname = localStorage.getItem('surname')
     this.roles = localStorage.getItem('roles')
     this.profilePic = localStorage.getItem('profilePic')
+    if (this.profilePic == 'none')
+      this.cover = '../../../assets/STAT-logo.png'
+    else
+      this.cover = this.profilePic
+
+    let el = document.getElementById('picProgress')
+    el.setAttribute('hidden', 'true')
   }
 
   openFile() {
@@ -38,11 +46,14 @@ export class ProfileComponent implements OnInit {
   downloadURL: Observable<string>;
 
   handle(event : any){
+    let el = document.getElementById('picProgress')
+    el.removeAttribute('hidden')
     var n = Date.now();
     const file = event.target.files[0];
     const filePath = `profilePictures/${n}`;
     const fileRef = this.storage.ref(filePath);
     const task = this.storage.upload(`profilePictures/${n}`, file);
+    this.uploadProgress = task.percentageChanges()
     task
       .snapshotChanges()
       .pipe(
@@ -52,6 +63,7 @@ export class ProfileComponent implements OnInit {
             if (url) {
               this.fb = url;
               this.profilePic = url
+              this.cover = url
               localStorage.setItem('profilePic', url)
               this.addPic(url)
             }
@@ -70,7 +82,8 @@ export class ProfileComponent implements OnInit {
     let req ={"profilePicture": pic}
     this.service.addProfilePicture(localStorage.getItem('token'),req).subscribe((data) => {
       console.log(data);
-
+      let el = document.getElementById('picProgress')
+      el.setAttribute('hidden', 'true')
     },
     error => {
       //console.log(error);
