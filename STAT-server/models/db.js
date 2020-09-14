e1a4f2a17f68639e4a1573f629c8d9a235cb9646
chirgugh <u17306192@tuks.co.za>
2020-09-14 09:26:58 +0200

    without decoding process.env.MONGODB_URI

--- STAT-server/models/db.js
+++ STAT-server/models/db.js
@@ -25,7 +25,7 @@ require("../config/config.js");
 mongoose.set('useNewUrlParser', true);
 
 console.log("check if mongourl exists --------------------- "+ process.env.MONGODB_URI)
-mongoose.connect(decodeURIComponent(process.env.MONGODB_URI), (err) => {
+mongoose.connect(process.env.MONGODB_URI, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
