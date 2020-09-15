c28a305cb1f5cfae25d53076e48be29f9d845161
chirgugh <u17306192@tuks.co.za>
2020-09-15 08:56:15 +0200

    update db and config.js

--- STAT-server/models/db.js
+++ STAT-server/models/db.js
@@ -28,7 +28,7 @@ console.log("check if mongouri exists---------------- "+process.env.MONGODB_URI+
 
 ///decodeURIComponent(process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI, (err) => {
+mongoose.connect(decodeURIComponent(process.env.MONGODB_URI), (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
