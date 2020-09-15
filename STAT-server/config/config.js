891cf9709f80746519be2a5937a37aa264c5cd66
chirgugh <u17306192@tuks.co.za>
2020-09-15 20:34:00 +0200

    run without coverage

--- STAT-server/config/config.js
+++ STAT-server/config/config.js
@@ -23,8 +23,8 @@
 // check env.
 var env = process.env.NODE_ENV || 'development';
 // fetch env. config
-var config = require('./config.json');
-//var config = require('./conf.json'); 
+//var config = require('./config.json');
+var config = require('./conf.json'); 
 var envConfig = config[env];
 // add env. config values to process.env
 Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
