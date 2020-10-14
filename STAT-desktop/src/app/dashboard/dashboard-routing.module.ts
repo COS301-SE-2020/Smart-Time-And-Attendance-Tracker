23e06bea055a3499ce31b57a3c93b7f1fc76ae00
Vianka213 <u18278273@tuks.co.za>
2020-10-14 18:35:25 +0200

    Removed calendar from desktop

--- STAT-desktop/src/app/dashboard/dashboard-routing.module.ts
+++ STAT-desktop/src/app/dashboard/dashboard-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { TodayComponent } from './today/today.component';
-import { CalendarComponent } from './calendar/calendar.component';
 import { OrganisationComponent } from './organisation/organisation.component';
 import { TeamsComponent } from './teams/teams.component';
 import { HistoryComponent } from "./history/history.component";
@@ -16,7 +15,6 @@ const routes: Routes = [
   { path: 'main', component: MainComponent },
   { path: 'unauthorised', component: UnauthorisedComponent },
   { path: 'today', component: TodayComponent},
-  { path: 'calendar', component: CalendarComponent},
   { path: 'iot', component: IOTComponent},
   { path: 'organisation', component: OrganisationComponent},
   { path: 'teams', component: TeamsComponent},
--- STAT-desktop/src/app/dashboard/dashboard.module.ts
+++ STAT-desktop/src/app/dashboard/dashboard.module.ts
@@ -6,7 +6,6 @@ import { MainComponent } from './main/main.component';
 import { MaterialComponentsModule } from '../material-components/material-components.module';
 import { MatIconModule } from '@angular/material/icon';
 import { TodayComponent } from './today/today.component';
-import { CalendarComponent } from './calendar/calendar.component';
 import { IOTComponent } from './iot/iot.component';
 import { OrganisationComponent } from './organisation/organisation.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -29,7 +28,7 @@ import { AttendanceComponent } from './attendance/attendance.component';
 
 
 @NgModule({
-  declarations: [MainComponent, TodayComponent, OrganisationComponent, ProjectsComponent, TeamsComponent, HistoryComponent, CalendarComponent, IOTComponent, UnauthorisedComponent, AnalysisComponent, ProfileComponent, AttendanceComponent],
+  declarations: [MainComponent, TodayComponent, OrganisationComponent, ProjectsComponent, TeamsComponent, HistoryComponent, IOTComponent, UnauthorisedComponent, AnalysisComponent, ProfileComponent, AttendanceComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
